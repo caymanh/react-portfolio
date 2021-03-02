2f0a50e41f413069f725f312639073bce3570593
Cayman Heng <hengcayman@gmail.com>
2021-03-02 09:27:10 -0500

    changed class to className for css styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,34 @@ import React from "react";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white">
-    <a className="navbar-brand" href="/">Cayman Heng</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-item nav-link" href="/about">About</a>
-        <a className="nav-item nav-link" href="/project">Project</a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Cayman Heng
+        </a>
+        <button
+        className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="/about">
+              About
+            </a>
+            <a className="nav-link" href="/project">
+              Project
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
 
