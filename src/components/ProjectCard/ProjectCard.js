eeb7e5b3dc121751d8362ff5350963987005c47b
Cayman Heng <hengcayman@gmail.com>
2021-03-05 10:00:39 -0500

    created direcotry for project card component and imported it to project page

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProjectCard.css";
+
+function ProjectCard() {
+  return "This is the ProjectCard component";
+}
+
+export default ProjectCard;
--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import ProjectCard from "../../components/ProjectCard/ProjectCard"
 import Footer from "../../components/Footer/Footer";
 
 function Project() {
   return (
     <div>
       <Header />
-      This is the Project Page
+      <p className="fs-1 fw-bold mt-5">My Recent Projects</p>
+      <p>Here are a few projects that I have worked on recently.</p>
+      <ProjectCard/>
       <Footer />
     </div>
   );
