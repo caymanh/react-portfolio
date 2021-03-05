c08437cb4d1051a107e9f4e6ce753ab545ccf918
Cayman Heng <hengcayman@gmail.com>
2021-03-05 17:01:17 -0500

    added props and css styles to project card component

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,21 +3,30 @@ import "./ProjectCard.css";
 
 function ProjectCard(props) {
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4 my-5">
-      <div className="col">
-        <div className="card h-100">
-          <img src={props.image} className="card-img-top" alt="" />
-          <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
-            <p className="card-text">{props.description}</p>
-            <a href={props.button} class="btn btn-primary">
-              View Project
-            </a>
-            <a href={props.sideButton} class="btn btn-primary">
-              Github Repository 
-            </a>
-          </div>
+    <div className="col mb-5">
+      <div className="card h-100">
+        <img src={props.image} className="card-img-top h-100" alt="" />
+        <div className="card-body">
+          <h5 className="card-title my-2">{props.title}</h5>
+          <p className="card-text text-align my-3">{props.description}</p>
+          <a
+            href={props.button}
+            className="btn btn-color text-white fw-bold"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View Project
+          </a>
+          <a
+            href={props.sideButton}
+            className="btn sub-title fw-bold"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github Repository
+          </a>
         </div>
+        <div class="card-footer text-muted fw-light">{props.tech}</div>
       </div>
     </div>
   );
