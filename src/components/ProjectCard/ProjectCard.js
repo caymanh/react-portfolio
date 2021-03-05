46643e761a7f43af5eaa17e8a38ba5abbddf6051
Cayman Heng <hengcayman@gmail.com>
2021-03-05 10:35:10 -0500

    added template to project card component

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./ProjectCard.css";
 
-function ProjectCard() {
-  return "This is the ProjectCard component";
+function ProjectCard(props) {
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-4 my-5">
+      <div className="col">
+        <div className="card h-100">
+          <img src={props.image} className="card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+            <a href={props.buttonHref} class="btn btn-primary">
+              {props.buttonText}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import ProjectCard from "../../components/ProjectCard/ProjectCard"
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import Footer from "../../components/Footer/Footer";
 
 function Project() {
   return (
-    <div>
+    <div className="yellow">
       <Header />
-      <p className="fs-1 fw-bold mt-5">My Recent Projects</p>
-      <p>Here are a few projects that I have worked on recently.</p>
-      <ProjectCard/>
+      <div className="container">
+        <p className="fs-1 fw-bold mt-5">My Recent Projects</p>
+        <p>Here are a few projects that I have worked on recently.</p>
+        <ProjectCard />
+      </div>
+
       <Footer />
     </div>
   );
