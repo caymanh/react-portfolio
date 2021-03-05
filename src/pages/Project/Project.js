53e7ad1dcc3c6a1734d0b3ee5854d6905983ff7a
Cayman Heng <hengcayman@gmail.com>
2021-03-05 09:40:34 -0500

    imported footer component to project page

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function Project() {
   return (
     <div>
       <Header />
       This is the Project Page
+      <Footer />
     </div>
   );
 }
